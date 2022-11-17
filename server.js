1c1378579a2bb4ae19eebb57753485fdc251148c
Amit Kumar <72488615+amitsvg@users.noreply.github.com>
2022-11-17 13:22:50 +0530

    added path.name

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.put("/update/:id", (req, res) => {
 });
 
 if(process.env.NODE_ENV ==="production"){
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join("client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
